ea367a373384012cc4a1688323153a9aa0d157d2
W. G. Ackworth <wgackworth@proton.me>
2024-05-30 13:03:30 +0200

    Adding a utility function for printing engine and version info

---
+++ include/MK/Core/Constants.h
@@ -0,0 +1,21 @@
+#ifndef MK_CONSTANTS_H
+#define MK_CONSTANTS_H
+
+/**
+ * @brief Name of the MK engine.
+ */
+extern const char* MK_ENGINE_NAME;
+/**
+ * @brief Version of the MK engine.
+ */
+extern const char* MK_ENGINE_VERSION;
+/**
+ * @brief Author of the MK engine.
+ */
+extern const char* MK_ENGINE_AUTHOR;
+/**
+ * @brief License information for the MK engine.
+ */
+extern const char* MK_ENGINE_LICENSE;
+
+#endif // MK_CONSTANTS_H
