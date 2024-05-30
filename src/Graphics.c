ed905a6b3ea102d506353a6d0f470e65d51a051e
W. G. Ackworth <wgackworth@proton.me>
2024-05-30 13:03:29 +0200

    Creating the RGBA struct

--- src/Graphics.c
+++ src/Graphics.c
@@ -14,3 +14,13 @@ void mk_gfx_useFillMode()
 {
   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
+
+void mk_gfx_useClearColor(const mk_RGBA clearColor)
+{
+  glClearColor(
+    clearColor.red,
+    clearColor.green,
+    clearColor.blue,
+    clearColor.alpha
+  );
+}
