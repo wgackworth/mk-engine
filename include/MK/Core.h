7960669e6d1a900f90a312ae05ade9a2d97d6274
W. G. Ackworth <wgackworth@proton.me>
2024-05-30 13:03:31 +0200

    Adding initialization functions

--- include/MK/Core.h
+++ include/MK/Core.h
@@ -4,10 +4,27 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "Core/Constants.h"
 #include "Core/File.h"
 #include "Core/Color.h"
+
+/**
+ * @brief Initializes GLFW.
+ * @return True if initialization is successful, false otherwise.
+ */
+bool mk_initializeGlfw();
+/**
+ * @brief Initializes GLEW.
+ * @return True if initialization is successful, false otherwise.
+ */
+bool mk_initializeGlew();
+
+/**
+ * @brief Terminates GLFW.
+ */
+void mk_terminate();
 
 /**
  * @brief Prints information about the MK engine.
